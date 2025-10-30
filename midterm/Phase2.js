b608be425b3f1220680eeb1da7be6b2f063a89c2
aerwin-wq <aerwin@berklee.edu>
2025-10-30 13:53:52 -0400

    Create Phase2.js

---
+++ midterm/Phase2.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(150, 150);
+  background(255);
+  stroke(0);
+  strokeWeight(6);
+  noFill();
+
+  ellipse(75, 95, 80, 64);
+
+  strokeWeight(5);
+  line(55, 88, 95, 104);
+  line(60, 105, 90, 85);
+
+  strokeWeight(10);
+  line(115, 25, 115, 120);
+
+  strokeWeight(6);
+  line(112, 115, 120, 108);
+  line(37, 118, 22, 130);
+}
